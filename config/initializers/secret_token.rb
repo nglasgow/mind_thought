90ccf6cabc6d35ed809c7ee75fb8df6c7b024329
nglasgow <nicole.glasgow@ymail.com>
2013-07-23 10:22:48 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MindThoughts::Application.config.secret_token = '8740705fcc1f2d2183a6754baf68e94c27885a2aa2cf38a5f0ddd86ca89d235277c76a5459da354230d651fd9d19259d4f4821dd8d99dc5fd93fa08541c48517'
