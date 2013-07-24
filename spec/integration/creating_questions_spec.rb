82bf800bce951d18330d06700b69418ebff6b65e
nglasgow <nicole.glasgow@ymail.com>
2013-07-23 18:09:55 -0700

    Add title functionality for show page

--- spec/integration/creating_questions_spec.rb
+++ spec/integration/creating_questions_spec.rb
@@ -8,5 +8,9 @@ feature 'Creating Questions' do
   fill_in 'Answer', :with => "A text-editor for OS X"
   click_button 'Create Question'
   page.should have_content('Question has been created.')
+  question = Question.find_by_inquiry("TextMate 2")
+  page.current_url.should == question_url(question)
+  title = "TextMate 2 - Questions - Mind_Thought"
+  find("title").should have_content(title)
   end
 end
